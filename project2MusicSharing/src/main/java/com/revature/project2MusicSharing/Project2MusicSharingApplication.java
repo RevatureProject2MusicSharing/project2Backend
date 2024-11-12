7a104e53cc01d026be10275935e57ab3b70ef28a
Hengrui Jia <hari20020511@gmail.com>
2024-11-12 15:30:27 -0500

    Set Up Backend Project
    Set Up Spring

---
+++ project2MusicSharing/src/main/java/com/revature/project2MusicSharing/Project2MusicSharingApplication.java
@@ -0,0 +1,13 @@
+package com.revature.project2MusicSharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2MusicSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2MusicSharingApplication.class, args);
+	}
+
+}
---
+++ project2MusicSharing/src/test/java/com/revature/project2MusicSharing/Project2MusicSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.project2MusicSharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2MusicSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
