1bdc0d0da800d895c22687be81ec4a9dd3676eb4
phamM302 <phammichael1001@tamu.edu>
2024-11-13 16:16:21 -0600

    added some song service methods add findall random and the dao

---
+++ project2MusicSharing/src/main/java/com/revature/services/SongService.java
@@ -0,0 +1,50 @@
+package com.revature.services;
+
+import com.revature.daos.SongDAO;
+import com.revature.models.Song;
+import com.revature.models.dtos.IncomingSongDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SongService {
+
+    private SongDAO songDAO;
+    //private PlaylistDAO playlistDAO;
+
+
+    @Autowired
+    public SongService(SongDAO songDAO) {
+        this.songDAO = songDAO;
+    }
+    /*@Autowired
+    public SongService(SongDAO songDAO, PlaylistDAO, playlistDAO) {
+        this.songDAO = songDAO;
+        this.playlistDAO = playlistDAO;
+    }*/
+
+    public Song addSong(IncomingSongDTO incomingSongDTO) {
+        Song newSong = new Song(0, incomingSongDTO.getSongName(),
+                incomingSongDTO.getYoutubeLink(), incomingSongDTO.getGenre(), null);
+        songDAO.save(newSong);
+        return newSong;
+    }
+
+    public List<Song> getAllSongs() { return songDAO.findAll(); }
+
+    public Song removeSongById(int songId) {
+        Song songToDelete = songDAO.findById(songId).orElseThrow(() ->
+                new IllegalArgumentException("No song found with id: " + songId));
+
+        songDAO.deleteById(songId);
+        return songToDelete;
+    }
+
+    public Song randomSongSelector() {
+        return songDAO.findRandomSong().orElseThrow(() ->
+                new IllegalArgumentException("No songs available"));
+    }
+
+}
